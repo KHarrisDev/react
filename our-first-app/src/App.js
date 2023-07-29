09c3081f6b2644473b930b0c93ac49697cdac44d
KHarrisDev <kharrisjr.dev@gmail.com>
2023-07-29 19:27:15 -0400

    CSS Modules

--- our-first-app/src/App.js
+++ our-first-app/src/App.js
@@ -2,8 +2,9 @@
 // import BEM from './components/BEM';
 // import UseStateExample from './components/UseStateExample';
 // import ClassComponentExample from './components/ClassComponentExample';
-import PropsExample from './components/PropsExample';
-import './App.css';
+// import PropsExample from './components/PropsExample';
+// import styles from './App.module.css';
+// // In the code below, we have the same className error but since it's in a different module, the style from this sheet is not the same
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
       {/* <BEM /> */}
       {/* <UseStateExample /> */}
       {/* <ClassComponentExample /> */}
-      <PropsExample />
+      {/* <PropsExample /> */}
+      {/* <h1>This following H2 tag is an example of CSS Modules.</h1>
+      <h2 className={styles.error}>This is an error</h2>
+      <h3 className={styles.variable__error}>This is an example a CSS Variable located in index.css</h3> */}
     </div>
   );
 }
--- our-first-app/src/components/Product.js
+++ our-first-app/src/components/Product.js
@@ -1,10 +1,11 @@
 import ItemDescription from './ItemDescription';
-import "./Product.css";
+import styles from './Product.module.css';
+// CSS Modules uses dot notation when declaring CSS classes to avoid conflicting styles in components. See below
 
 // The following code is an example of passing props "down" to another component without destructuring the props.
 function Product(props) {
     return (
-        <div>
+        <div className={styles.product}>
             <ItemDescription name={props.name} description={props.description}/>
             <h1>There was an error.</h1>
             <h3>{props.price}</h3>
